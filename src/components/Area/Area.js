5902df06f04f25f6010af97d55c45a9a793e17e6
FAHIM AHMED <fahimfahad35@gmail.com>
2020-08-27 19:36:02 +0600

    bootstrap added

--- src/components/Area/Area.js
+++ src/components/Area/Area.js
@@ -14,13 +14,16 @@ const enrollControll=(course)=>{
     setCart(addedCourse);
 }
     return (
-        <div className="area">
-            <div className="course-area">
+        <div className="area container">
+            <div className="row">
+            <div className="course-area col-7 col-sm-7 col-md-10 col-lg-10">
                {course.map(item =><Course sent={item} key={item.id} enroll={enrollControll}></Course>)}
              </div>
-            <div className="cart-area">
+            <div className="cart-area col-2 col-sm-2 col-md-2 col-lg-2">
             <Cart cart={cart}></Cart>
             </div>
+            </div>
+            
         </div>
     );
 };
--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -6,21 +6,25 @@ import { faUnlock} from '@fortawesome/free-solid-svg-icons'
 const Course = (props) => {
     const{name,title,price,image}=props.sent;
     return (
-        <div className="total-area">
-            <div className="image-area">
+        <div className="total-area container">
+            <div className="row">
+            <div className="image-area col-10 col-sm-10 col-md-4 col-lg-4">
             <img src={image} alt=""/>
             </div>
-            <div className="info-area">
+            <div className="info-area col-10 col-sm-10 col-md-8 col-lg-8">
             <h4>{name}</h4>
              <p>{title}</p>
              <br/>
              <h2 style={{fontSize:'bold'}}>${price}</h2>
-             <button className="enroll-button"
+             <button className="enroll-button btn btn-success"
                 onClick={()=>props.enroll(props.sent)}             
              >
                  <FontAwesomeIcon icon={faUnlock} /> Enroll Now
                  </button>
             </div>       
+
+            </div>
+           
         </div>
     );
 };
